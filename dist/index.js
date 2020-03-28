3517adb6f09571efdfc90bdecb6957ffa506d647
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-03-28 17:50:53 +0100

    creating folders & logging

--- dist/index.js
+++ dist/index.js
@@ -126,10 +126,16 @@ const fs = __webpack_require__(747);
 try{
     const url = core.getInput('url');
     const file = core.getInput('file');
-    const success = fetch(url)
-        .then(response => response.json())
+    fetch(url)
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            core.setFailed(`fetch to ${url} failed with status: ${response.status}`);
+        })
         .then(data => {
             fs.writeFileSync(file, data);
+            console.log(`successfully saved data from ${url} to ${file}`);
         });
     if(!success){
         core.setFailed('fetch failed');
--- index.js
+++ index.js
@@ -2,14 +2,39 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const path = require('path')
+
+const makeSureFoldersAreCreated = filename => {
+    const folders = filename.split(path.sep).slice(0, -1)
+    if (folders.length) {
+        folders.reduce((last, folder) => {
+            const folderPath = last ? last + path.sep + folder : folder
+            if (!fs.existsSync(folderPath)) {
+                fs.mkdirSync(folderPath)
+            }
+            return folderPath
+        })
+    }
+}
 
 try{
     const url = core.getInput('url');
     const file = core.getInput('file');
-    const success = fetch(url)
-        .then(response => response.json())
+    console.log(`Fetch data started with`, `url: ${url}`, `file: ${file}`);
+    if(!url){
+        core.setFailed('url required');
+    }
+    fetch(url)
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            core.setFailed(`fetch to ${url} failed with status: ${response.status}`);
+        })
         .then(data => {
+            makeSureFoldersAreCreated(file);
             fs.writeFileSync(file, data);
+            console.log(`successfully saved data from ${url} to ${file}`);
         });
     if(!success){
         core.setFailed('fetch failed');
